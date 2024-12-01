bf31cc0afa327e2d9a25ea55e5653e97f040eb17
OmerAqeel <omrraqeel@gmail.com>
2024-12-01 15:27:30 +0000

    fixed the configuration issues of the Vite app and initiated the Sign-up page for the app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import { SignIn } from './Pages/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
