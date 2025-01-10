ad5d7d346f07091b5daa190e137bac63a092e9c4
OmerAqeel <omrraqeel@gmail.com>
2025-01-10 15:55:40 +0000

    dropdown for contact us design implemented

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import {React, useEffect, useState} from "react";
 import { VscHubot } from "react-icons/vsc";
 import "../Styles/Navbar.css";
 import { Link, useLocation } from "react-router-dom";
 import { User } from "lucide-react";
 import { ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import { Settings } from "lucide-react";
 import { UserPen } from "lucide-react";
 import { LogOut } from "lucide-react";
@@ -27,6 +28,22 @@ import { useParams } from "react-router-dom";
 
 export const Navbar = () => {
   const location = useLocation();
+  const [textEntered, setTextEntered] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setTextEntered(e.target.value);
+    setIsValid(true);
+  };
+
+  useEffect(() => {
+    if (textEntered.length === 0) {
+      setIsValid(false);
+    }
+    if(textEntered.length > 0){
+      setIsValid(true);
+    }
+  }, [textEntered]);
 
   // Extract the assistantId from the pathname
   const match = location.pathname.match(/\/chat\/([^/]+)/);
@@ -63,9 +80,67 @@ export const Navbar = () => {
         </Link>
       ) : location.pathname === "/dashboard" ? (
         <div className="dashboard-actions">
+          <DropdownMenu
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "6vw",
+          }}
+          >
+            <DropdownMenuTrigger asChild>
           <Button className="contact-btn" variant="outline">
             Contact us
           </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+            <DropdownMenuLabel
+             style={{
+              color: "gray",
+              fontSize: "12px",
+              fontWeight: "bold",
+              marginTop: "10px",
+            }}
+            >
+              Please write your query below:
+            </DropdownMenuLabel>
+              <DropdownMenuGroup>
+                  <Textarea
+                  style={{
+                    marginRight: "10px",
+                    marginLeft: "10px",
+                    marginTop: "10px",
+                    width: "20vw",
+                    height: "19vh",
+                    marginBottom: "10px",
+                    borderRadius: "10px",
+                    outline: "none",
+                    resize: "none",
+                  }}
+                  placeholder="Type here"
+                  value={textEntered}
+                  onChange={handleEmailChange}
+                  >
+                  </Textarea>
+
+                <Button
+                  style={{
+                    borderRadius: "10px",
+                    width: "6vw",
+                    outline: "none",
+                    fontSize: "12px",
+                    marginLeft: "210px",
+                    marginTop: "10px",
+                    marginBottom: "10px",
+                    backgroundColor: "#0284c7",
+                  }}
+                  disabled={!isValid}
+                >
+                  Submit
+                </Button>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button
